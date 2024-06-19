555035c603fb5b845dda0efd58b505d9341abca3
dqniel12396 <danielsanabria12396@gmail.com>
2024-06-18 22:14:02 -0500

    commit casi

--- src/main/java/dh/backend/clinicamvc/controller/VistaController.java
+++ src/main/java/dh/backend/clinicamvc/controller/VistaController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
+
 @Controller
 @RequestMapping("/vista")
 public class VistaController {
@@ -23,19 +25,19 @@ public class VistaController {
 
     @GetMapping("/buscarPaciente")
     public String buscarPacientePorId(Model model, @RequestParam Integer id){
-        Paciente paciente = pacienteService.buscarPorId(id);
+        Optional<Paciente> paciente = pacienteService.buscarPorId(id);
         model.addAttribute("especialidad", "Paciente");
-        model.addAttribute("nombre", paciente.getNombre());
-        model.addAttribute("apellido", paciente.getApellido());
+        model.addAttribute("nombre", paciente.get().getNombre());
+        model.addAttribute("apellido", paciente.get().getApellido());
         return "index";
     }
 
     @GetMapping("/buscarOdontologo")
     public String buscarOdontologoPorId(Model model, @RequestParam Integer id){
-        Odontologo odontologo = odontologoService.buscarUnOdontologo(id);
+        Optional<Odontologo> odontologo = odontologoService.buscarUnOdontologo(id);
         model.addAttribute("especialidad", "odontologo");
-        model.addAttribute("nombre", odontologo.getNombre());
-        model.addAttribute("apellido", odontologo.getApellido());
+        model.addAttribute("nombre", odontologo.get().getNombre());
+        model.addAttribute("apellido", odontologo.get().getApellido());
         return "index";
     }
 }
